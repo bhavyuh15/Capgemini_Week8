db76a2435e11f7486466491c40008bd786955a44
bhavyuh15 <bhavyadatey15@gmail.com>
2025-03-05 16:53:29 +0530

    [Bhavya] Added PIN CODE UC2 in PINCODE branch

--- PINCODE/PinCode.js
+++ PINCODE/PinCode.js
@@ -1,5 +1,6 @@
 function isValidPinCode(pin) {
-    let pinRegex = /^400088$/; // Exact match for 400088
+    let pinRegex = /^[0-9]{6}$/; // Exact match for 400088
     return pinRegex.test(pin);
 }
 console.log(isValidPinCode("400088")); 
+console.log(isValidPinCode("A40008")); 
