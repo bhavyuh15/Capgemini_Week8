f53c29bb16d8401e2ca5c0ecd8d646f6e496dcb7
bhavyuh15 <bhavyadatey15@gmail.com>
2025-03-05 16:56:28 +0530

    [Bhavya] Added PIN CODE UC3 in PINCODE branch

--- PINCODE/PinCode.js
+++ PINCODE/PinCode.js
@@ -1,6 +1,9 @@
 function isValidPinCode(pin) {
-    let pinRegex = /^[0-9]{6}$/; // Exact match for 400088
-    return pinRegex.test(pin);
+    let pinRegex = /^[0-9]{6}$/; // Ensures exactly 6 digits, no alphabets or special characters
+    return pinRegex.test(pin) 
 }
-console.log(isValidPinCode("400088")); 
-console.log(isValidPinCode("A40008")); 
+
+// Test cases
+console.log(isValidPinCode("400088"));  
+console.log(isValidPinCode("A400088")); 
+console.log(isValidPinCode("400088B")); 
