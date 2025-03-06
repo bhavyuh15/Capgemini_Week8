2444a0a26bbd36c4f5ac9907de9c4e0766b911b6
bhavyuh15 <bhavyadatey15@gmail.com>
2025-03-06 10:48:13 +0530

    [Bhavya] Added Email UC1 in Email  Branch

---
+++ Email/Email.js
@@ -0,0 +1,20 @@
+function validateEmail(email) {
+    const regex = /^abc(\.[a-zA-Z]+)?@bridgelabz\.co(\.[a-zA-Z]+)?$/;
+    return regex.test(email);
+}
+
+// Test cases
+const testEmails = [
+    "abc@bridgelabz.co",         // Valid
+    "abc.xyz@bridgelabz.co",     // Valid
+    "abc@bridgelabz.co.in",      // Valid
+    "abc.xyz@bridgelabz.co.in",  // Valid
+    "abcd@bridgelabz.co",        // Invalid (should start with abc)
+    "abc@xyz.co",                // Invalid (should contain bridgelabz)
+    "abc@bridgelabz.com",        // Invalid (should be .co)
+    "abc@bridgelabz.co.uk"       // Invalid (only .in is optional)
+];
+
+testEmails.forEach(email => {
+    console.log(`${email}: ${validateEmail(email)}`);
+});
