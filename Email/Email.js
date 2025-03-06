9a8d605ffec5e55c0f2e36bd7a367a83f52e7fbf
bhavyuh15 <bhavyadatey15@gmail.com>
2025-03-06 12:10:58 +0530

    [Bhavya] Added Email UC2 in Email Branch

--- Email/Email.js
+++ Email/Email.js
@@ -1,5 +1,6 @@
 function validateEmail(email) {
-    const regex = /^abc(\.[a-zA-Z]+)?@bridgelabz\.co(\.[a-zA-Z]+)?$/;
+    const regex = /^[a-zA-Z0-9._%+-]+@bridgelabz\.[a-zA-Z]+$/
+
     return regex.test(email);
 }
 
